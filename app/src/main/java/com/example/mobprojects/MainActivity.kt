cdf08c4c09f3b85d58f258995ea55abe76644cfa
boba4k2 <boba4k@gmail.com>
2025-10-14 08:03:12 +0700

    I did a task with transitions between two activities

--- app/src/main/java/com/example/mobprojects/MainActivity.kt
+++ app/src/main/java/com/example/mobprojects/MainActivity.kt
@@ -1,48 +1,46 @@
 package com.example.mobprojects
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.example.mobprojects.ui.theme.MobProjectsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MobProjectsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-
-@Composable
-fun GreetingPreview() {
-    MobProjectsTheme {
-        Greeting("Android")
+fun MainScreen() {
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+    ) {
+        Text(text = "Вы находитесь на экране 1")
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = {
+            val intent = Intent(context, SecondActivity::class.java)
+            context.startActivity(intent)
+        }) {
+            Text(text = "Перейти на экран 2")
+        }
     }
 }
