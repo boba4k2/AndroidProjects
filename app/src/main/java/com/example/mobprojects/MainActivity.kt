38a9db465962fb868e70f1e8e277f045477ab261
boba4k2 <boba4k@gmail.com>
2025-10-21 01:21:30 +0700

    I did a task with login and password

--- app/src/main/java/com/example/mobprojects/MainActivity.kt
+++ app/src/main/java/com/example/mobprojects/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -11,7 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mobprojects.ui.theme.MobProjectsTheme
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.material3.Surface
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +35,29 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val context = LocalContext.current
+    val textState1 = remember { mutableStateOf("") }
+    val textState2 = remember { mutableStateOf("") }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFFFAEEFD)
+    ){}
+    Box(modifier = Modifier.fillMaxWidth()){
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .background(Color.Black)
+                .align(Alignment.TopStart)
+        )
+        Text(
+            text = "MobProject",
+            Modifier
+                .align(Alignment.CenterStart),
+            color = Color.White,
+            fontSize = 20.sp
+        )
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -34,13 +65,34 @@ fun MainScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
     ) {
-        Text(text = "Вы находитесь на экране 1")
+        Text(text = "You are on Activity1")
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            value = textState1.value,
+            onValueChange = { textState1.value = it },
+            label = { Text("Login") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            value = textState2.value,
+            onValueChange = { textState2.value = it },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth()
+        )
         Spacer(modifier = Modifier.height(20.dp))
         Button(onClick = {
-            val intent = Intent(context, SecondActivity::class.java)
-            context.startActivity(intent)
+            val inLogin = textState1.value
+            val inPassword = textState2.value
+            val intent = Intent(context, SecondActivity::class.java).apply {
+                putExtra("input_text", inLogin)
+            }
+            if ((inLogin == "123") and (inPassword == "123")){
+                context.startActivity(intent)
+            }
+
         }) {
-            Text(text = "Перейти на экран 2")
+            Text(text = "Go to SecondActivity")
         }
     }
 }
